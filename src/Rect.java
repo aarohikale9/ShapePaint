9e3f110061687fa010ecc51ef43356ccdc25da4e
mahikamaini <93836668+DuchessBanana@users.noreply.github.com>
2023-12-06 13:46:16 -0800

    random colors for rectangle on click

--- src/Rect.java
+++ src/Rect.java
@@ -2,15 +2,19 @@ import processing.core.PApplet;
 
 public class Rect extends Shape {
 
+    int red, green, blue;
     public Rect(int x, int y) {
         super(150, 50);
         this.x = x;
         this.y = y;
+        red = (int) (Math.random()*255);
+        green = (int) (Math.random()*255);
+        blue = (int) (Math.random()*255);
     }
 
 
     public void draw(PApplet window) {
-        window.fill(255,0, 0);
+        window.fill(red,green,blue);
         window.rect(x,y,width,height);
     }
 
